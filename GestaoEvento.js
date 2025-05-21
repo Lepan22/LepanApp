9b7d2dc9344e0ee0377512a37ae19ecd756196c0
Lepan22 <contato@lepanpan.com.br>
2025-05-21 09:36:10 -0300

    Update GestaoEvento.js

--- GestaoEvento.js
+++ GestaoEvento.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
   apiKey: "AIzaSyBClDBA7f9-jfF6Nz6Ia-YlZ6G-hx3oerY",
   authDomain: "lepanapp.firebaseapp.com",
@@ -28,6 +27,62 @@ function carregarClientes() {
   });
 }
 
+function carregarResponsaveis() {
+  const select = document.getElementById('responsavel');
+  db.ref('equipe').once('value').then(snapshot => {
+    select.innerHTML = '<option value="">Selecione</option>';
+    snapshot.forEach(child => {
+      const membro = child.val();
+      const opt = document.createElement('option');
+      opt.value = membro.apelido;
+      opt.textContent = membro.apelido;
+      select.appendChild(opt);
+    });
+  });
+}
+
+let equipeAlocada = [];
+let logisticaAlocada = [];
+
+function adicionarEquipe() {
+  equipeAlocada.push({ membroId: '', valor: 0 });
+  renderizarEquipe();
+}
+
+function renderizarEquipe() {
+  const container = document.getElementById('equipeContainer');
+  container.innerHTML = '';
+  equipeAlocada.forEach((item, i) => {
+    const div = document.createElement('div');
+    div.className = 'row mb-2';
+    div.innerHTML = `
+      <div class="col"><input type="text" class="form-control form-control-sm" placeholder="Membro" value="${item.membroId}" onchange="equipeAlocada[${i}].membroId = this.value"></div>
+      <div class="col"><input type="number" class="form-control form-control-sm" placeholder="Valor" value="${item.valor}" onchange="equipeAlocada[${i}].valor = parseFloat(this.value) || 0"></div>
+    `;
+    container.appendChild(div);
+  });
+}
+
+function adicionarLogistica() {
+  logisticaAlocada.push({ prestadorId: '', valor: 0 });
+  renderizarLogistica();
+}
+
+function renderizarLogistica() {
+  const container = document.getElementById('logisticaContainer');
+  container.innerHTML = '';
+  logisticaAlocada.forEach((item, i) => {
+    const div = document.createElement('div');
+    div.className = 'row mb-2';
+    div.innerHTML = `
+      <div class="col"><input type="text" class="form-control form-control-sm" placeholder="Prestador" value="${item.prestadorId}" onchange="logisticaAlocada[${i}].prestadorId = this.value"></div>
+      <div class="col"><input type="number" class="form-control form-control-sm" placeholder="Valor" value="${item.valor}" onchange="logisticaAlocada[${i}].valor = parseFloat(this.value) || 0"></div>
+    `;
+    container.appendChild(div);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   carregarClientes();
+  carregarResponsaveis();
 });
